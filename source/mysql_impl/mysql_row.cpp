2ae7bb8f6609abd89ebc7e4f124be1d4a7ae6e90
Hackman.Lo <admin@yytec.net>
2023-07-24 10:34:06 +0800

    Initialize

--- source/mysql_impl/mysql_row.cpp
+++ source/mysql_impl/mysql_row.cpp
@@ -23,6 +23,12 @@ namespace rosetta {
         core::sql_param_value value = nullptr;
         switch(bind_[column].buffer_type)
         {
+            case MYSQL_TYPE_LONG:
+            case MYSQL_TYPE_INT24:
+            case MYSQL_TYPE_SHORT:
+            case MYSQL_TYPE_LONGLONG:
+                value = *(unsigned long long *)data_[column].get();
+                break;
             case MYSQL_TYPE_BLOB:
                 value = std::string((const char*)data_[column].get(), strlen((const char*)data_[column].get()));
                 break;
@@ -33,13 +39,63 @@ namespace rosetta {
                 value = std::string((const char*)data_[column].get(), strlen((const char*)data_[column].get()));
                 break;
             case MYSQL_TYPE_TIMESTAMP:
-                MYSQL_TIME *time= (MYSQL_TIME *)data_[column].get();
+                auto *time= (MYSQL_TIME *)data_[column].get();
                 sahara::string str = sahara::string::static_format("{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{}", time->year, time->month, time->day, time->hour, time->minute, time->second, time->second_part);
                 sahara::time::timestamp ts = sahara::time::timestamp::from_string(str);
                 value = ts;
                 break;
-
-
+//            case MYSQL_TYPE_DECIMAL:
+//                break;
+//            case MYSQL_TYPE_TINY:
+//                break;
+//            case MYSQL_TYPE_FLOAT:
+//                break;
+//            case MYSQL_TYPE_DOUBLE:
+//                break;
+//            case MYSQL_TYPE_NULL:
+//                break;
+//            case MYSQL_TYPE_DATE:
+//                break;
+//            case MYSQL_TYPE_TIME:
+//                break;
+//            case MYSQL_TYPE_DATETIME:
+//                break;
+//            case MYSQL_TYPE_YEAR:
+//                break;
+//            case MYSQL_TYPE_NEWDATE:
+//                break;
+//            case MYSQL_TYPE_VARCHAR:
+//                break;
+//            case MYSQL_TYPE_BIT:
+//                break;
+//            case MYSQL_TYPE_TIMESTAMP2:
+//                break;
+//            case MYSQL_TYPE_DATETIME2:
+//                break;
+//            case MYSQL_TYPE_TIME2:
+//                break;
+//            case MYSQL_TYPE_TYPED_ARRAY:
+//                break;
+//            case MYSQL_TYPE_INVALID:
+//                break;
+//            case MYSQL_TYPE_BOOL:
+//                break;
+//            case MYSQL_TYPE_JSON:
+//                break;
+//            case MYSQL_TYPE_NEWDECIMAL:
+//                break;
+//            case MYSQL_TYPE_ENUM:
+//                break;
+//            case MYSQL_TYPE_SET:
+//                break;
+//            case MYSQL_TYPE_TINY_BLOB:
+//                break;
+//            case MYSQL_TYPE_MEDIUM_BLOB:
+//                break;
+//            case MYSQL_TYPE_LONG_BLOB:
+//                break;
+//            case MYSQL_TYPE_GEOMETRY:
+//                break;
         }
 
         return value;
--- test/main.cpp
+++ test/main.cpp
@@ -14,25 +14,21 @@ using namespace std::chrono_literals;
 
 int main() {
     rosetta::core::select bbb({"user_id", "username", "password"});
-    auto te = bbb.from({{"t_user", ""}}).where("username", "=", "?").where("deleted_at", "is", "null").inner_join(rosetta::core::alia{"t_user_access_token", "b"},[&](auto &join){
-        join.on("user_id", "=", "user_id");
+    auto te = bbb.from({{"t_user", ""}}).where("username", "=", "?").where("deleted_at", "is", "null").left_join(rosetta::core::alia{"t_user_access_token", "b"},[&](auto &join){
+        join.on("user_id", "=", "b.fn_user_id");
     });
     auto te1 = te.compile();
     auto te2 = te.compile();
 
     try {
-        rosetta::core::select select_builder({"user_id", "username", "password"});
-        select_builder.from({{"t_user", ""}}).where("username", "=", "?");
-
-
-        auto test = select_builder.compile();
+        auto test = bbb.compile();
 
         int a = 100;
         MYSQL *data = mysql_init(nullptr);
         data = mysql_real_connect(data, "127.0.0.1", "root", "hl97005497--", "science_society", 3306, nullptr, 0);
         //std::this_thread::sleep_for(std::chrono::seconds(30));
 
-        rosetta::database_pool pool(10, "127.0.0.1", 3306, "root", "hl97005497--", "science_society");
+        rosetta::database_pool pool(10, "127.0.0.1", 3306, "root", "hl97005497--", "go_member_sys");
         pool.initialize<rosetta::mysql_connection>();
         std::this_thread::sleep_for(1s);
         {
@@ -48,10 +44,9 @@ int main() {
         statement->execute();
         auto result = statement->get();
         auto row = result->next();
-        std::string uid = std::get<std::string>(row->get_column(0));
+        auto uid = std::get<std::uint64_t>(row->get_column(0));
         std::string uname = std::get<std::string>(row->get_column(1));
         std::string pwd = std::get<std::string>(row->get_column(2));
-        std::cout << uid.length() << ":" << uid.size() << std::endl;
         std::cout << uname.length() << ":" << uname.size() << std::endl;
         std::cout << pwd.length() << ":" << pwd.size() << std::endl;
         }
