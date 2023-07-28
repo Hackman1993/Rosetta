59a8d69cdb2085578e4b5475f6d68368c705be80
Hackman.Lo <admin@yytec.net>
2023-07-28 15:53:09 +0800

    Initialize

--- include/mysql.h
+++ include/mysql.h
@@ -5,4 +5,6 @@
 #ifndef ROSETTA_MYSQL_H
 #define ROSETTA_MYSQL_H
 #include "pool/mysql_impl/mysql_connection.h"
+#include "pool/mysql_impl/mysql_statement.h"
+#include "pool/mysql_impl/mysql_result.h"
 #endif //ROSETTA_MYSQL_H
