8fc4f0d0d1d92b641412f01c0e2512d7c140ae70
Hackman.Lo <admin@yytec.net>
2023-07-28 15:37:31 +0800

    Initialize

--- include/rosetta.h
+++ include/rosetta.h
@@ -4,9 +4,7 @@
 
 #ifndef OBELISK_ROSETTA_H
 #define OBELISK_ROSETTA_H
-#include "mysql_impl/mysql_connection.h"
-#include "mysql_impl/mysql_statement.h"
-#include "mysql_impl/mysql_result.h"
 #include "pool/sql_row.h"
 #include "database_pool.h"
+#include "builder/select.h"
 #endif //OBELISK_ROSETTA_H
